ce1888babdc2356d8996c8616111e392433593cc
sguenov <sguenov@hotmail.com>
2018-03-03 16:25:23 -0800

    better

--- bin/src/main.py
+++ bin/src/main.py
@@ -5,10 +5,3 @@
 # if new
 # text
 # new == false
-conn = pyodbc.connect(
-    r'DRIVER={ODBC Driver 13 for SQL Server};'
-    r'SERVER=test;'
-    r'DATABASE=test;'
-    r'UID=user;'
-    r'PWD=password'
-    )
